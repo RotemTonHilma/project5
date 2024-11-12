1fba1e05d040b9f0096d6fafa29381a7452dddfe
YairMoller <yairmoller@hilma.tech>
2024-11-12 17:56:00 +0200

    fix merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,7 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import LoggedUserProvider from "./context/LoggedUserContext";
-import Home from "./components/Home";
-
-function App() {
-
-  return (
-
-    <>
-      <LoggedUserProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Login />} />
-            <Route path="login" element={<Login />} />
-            {/* <Route path="register" element={<Register />} /> */}
-            <Route path="home" element={<Home />}>
-              {/* <Route />
-            <Route />
-            <Route /> */}
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </LoggedUserProvider>
-    </>
-  )
-=======
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Login from "./pages/login/Login.jsx";
 import Register from "./pages/login/Register.jsx";
 import UserLayout from "./components/UserLayout";
 import Info from "./pages/Info.jsx";
@@ -39,32 +12,35 @@ import ProtectedRoutes from "./components/ProtectedRoutes.jsx";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Login />} />
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
+        <>
+            <LoggedUserProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index element={<Login />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="register" element={<Register />} />
 
-                <Route
-                    path="user/:userId"
-                    element={
-                        <ProtectedRoutes>
-                            <UserLayout />
-                        </ProtectedRoutes>
-                    }
-                >
-                    <Route index element={<Home />} />
-                    <Route path="home" element={<Home />} />
-                    <Route path="info" element={<Info />} />
-                    <Route path="todos" element={<Todos />} />
-                    <Route path="posts" element={<Posts />} />
-                    <Route path="albums" element={<Albums />} />
-                </Route>
-                <Route path="*" element={<h1>Error</h1>} />
-            </Routes>
-        </BrowserRouter>
+                        <Route
+                            path="user/:userId"
+                            element={
+                                <ProtectedRoutes>
+                                    <UserLayout />
+                                </ProtectedRoutes>
+                            }
+                        >
+                            <Route index element={<Home />} />
+                            <Route path="home" element={<Home />} />
+                            <Route path="info" element={<Info />} />
+                            <Route path="todos" element={<Todos />} />
+                            <Route path="posts" element={<Posts />} />
+                            <Route path="albums" element={<Albums />} />
+                        </Route>
+                        <Route path="*" element={<h1>Error</h1>} />
+                    </Routes>
+                </BrowserRouter>
+            </LoggedUserProvider>
+        </>
     );
->>>>>>> main
 }
 
 export default App;
