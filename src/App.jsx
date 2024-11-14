cd9f7a70e11ad9d1e9466487dd46c9ea17c6bdd2
RotemTon <rotemton@hilma.tech>
2024-11-14 10:37:49 +0200

    add registration navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/Login";
 import LoggedUserProvider from "./context/LoggedUserContext";
 import Home from "./pages/Home.jsx";
@@ -11,42 +11,47 @@ import Albums from "./pages/albums/Albums.jsx";
 import ProtectedRoutes from "./components/ProtectedRoutes.jsx";
 import RegDetails from "./pages/login/RegDetails.jsx";
 import Photos from "./pages/albums/Photos.jsx";
+import RegistrationLayout from "./components/RegistrationLayout.jsx";
+
 function App() {
-    return (
-        <>
-            <LoggedUserProvider>
-                <BrowserRouter>
-                    <Routes>
-                        <Route index element={<Login />} />
-                        <Route path="login" element={<Login />} />
-                        <Route path="register" element={<Register />} />
-                        <Route path="register/details" element={<RegDetails />} />
+  return (
+    <>
+      <LoggedUserProvider>
+        <BrowserRouter>
+          <Routes>
 
-                        <Route
-                            path="user/:userId"
-                            element={
-                                <ProtectedRoutes>
-                                    <UserLayout />
-                                </ProtectedRoutes>
-                            }
-                        >
-                            <Route index element={<Home />} />
-                            <Route path="home" element={<Home />} />
-                            <Route path="info" element={<Info />} />
-                            <Route path="todos" element={<TodosDisplay />} />
-                            <Route path="posts" element={<Posts />} />
+            <Route path="/" element={<RegistrationLayout />} >
+              <Route index element={<Navigate to="login" replace />} />
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="register/details" element={<RegDetails />} />
+            </Route>
 
-                            <Route path="albums">
-                                <Route index element={<Albums />} />
-                                <Route path=":albumId/photos" element={<Photos />} />
-                            </Route>
-                        </Route>
-                        <Route path="*" element={<h1>Error</h1>} />
-                    </Routes>
-                </BrowserRouter>
-            </LoggedUserProvider>
-        </>
-    );
+            <Route
+              path="user/:userId"
+              element={
+                <ProtectedRoutes>
+                  <UserLayout />
+                </ProtectedRoutes>
+              }
+            >
+              <Route index element={<Home />} />
+              <Route path="home" element={<Home />} />
+              <Route path="info" element={<Info />} />
+              <Route path="todos" element={<TodosDisplay />} />
+              <Route path="posts" element={<Posts />} />
+
+              <Route path="albums">
+                <Route index element={<Albums />} />
+                <Route path=":albumId/photos" element={<Photos />} />
+              </Route>
+            </Route>
+            <Route path="*" element={<h1>Error</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </LoggedUserProvider>
+    </>
+  );
 }
 
 export default App;
